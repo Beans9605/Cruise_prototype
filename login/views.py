35586241f6fec24be11bb35d363faa6e2db56d1a
Bean9605 <alkad1234@likelion.org>
2020-03-05 17:54:14 +0900

    home_re

--- login/views.py
+++ login/views.py
@@ -1,9 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from mainsite.models import User
 
 # Create your views here.
 
 def sign_up (request) :
     return render(request, 'login/sign.html')
+
+def sign_up_indata (request) :
+    username = request.POST.get('username')
+    password = request.POST.get('pwd')
+    email = request.POST.get('email')
+    phone = request.POST.get('phone')
+    gender = request.POST.get('gender')
+    birthday = request.POST.get('birth')
+    firstname = request.POST.get('Fname')
+    lastname = request.POST.get('Lname')
+
+    if User.objects.filter(username = username)
+    
+
 
 def sign_in (request) :
     return render(request, 'login/login.html')
